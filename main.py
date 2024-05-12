59e7f45deca37d7b8de87187dfad30734d3d3f0d
tri <trikaydev@gmail.com>
2024-05-13 00:50:53 +0700

    update re

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from fastapi import FastAPI
 from Logic.ModelPredict import ModelPredict
 
 app = FastAPI()
-
-
 @app.route('/api/data', methods=['GET'])
 def get_data():
     model = ModelPredict()
     signal = model.run()
     return signal
+
+@app.route('/get_signal', methods=['GET'])
+def get_data_signal():
+    # model = ModelPredict()
+    # signal = model.run()
+    return "signal"
